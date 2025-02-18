cf3949ed6d86f97c7c91db220a55d7ed6b096f26
Cesar23-eng <a2023114080@estudiantes.upsa.edu.bo>
2025-02-17 21:25:50 -0400

    Initial commit

---
+++ CaculadoraAPI/Controllers/CalculadoraController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CaculadoraAPI.Controllers;
+
+public class CalculadoraController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
